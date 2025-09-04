7cb584fb10b7bc43360de41d6122bca2dfe8be18
Germán De Francesco <germandefrancesco@hotmail.com>
2025-09-04 00:09:02 -0300

    Separate Models and Views

---
+++ src/MauiXaml/Views/NotePage.xaml.cs
@@ -0,0 +1,42 @@
+namespace MauiXaml.Views;
+
+public partial class NotePage : ContentPage
+{
+    string _fileName = Path.Combine(FileSystem.AppDataDirectory, "notes.txt");
+
+    public NotePage()
+    {
+        InitializeComponent();
+
+        var appDataPath = FileSystem.AppDataDirectory;
+        var randomFileName = $"{Path.GetRandomFileName()}.notes.txt";
+        LoadNote(Path.Combine(appDataPath, randomFileName));
+    }
+
+    private void SaveButton_Clicked(object sender, EventArgs e)
+    {
+        File.WriteAllText(_fileName, TextEditor.Text);
+    }
+
+    private void DeleteButton_Clicked(object sender, EventArgs e)
+    {
+        if (File.Exists(_fileName))
+            File.Delete(_fileName);
+
+        TextEditor.Text = string.Empty;
+    }
+
+    private void LoadNote(string fileName)
+    {
+        Models.Note noteModel = new Models.Note();
+        noteModel.Filename = fileName;
+
+        if (File.Exists(fileName))
+        {
+            noteModel.Date = File.GetCreationTime(fileName);
+            noteModel.Text = File.ReadAllText(fileName);
+        }
+
+        BindingContext = noteModel;
+    }
+}
